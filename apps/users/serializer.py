03829e5d084637a7cff84e5c6ada2e464610fe37
jorgegordillodev <jorgegordillo.dev@gmail.com>
2022-07-11 23:27:34 -0500

    cambio

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -1,7 +1,6 @@
 from rest_framework.serializers import Serializer, ModelSerializer, CharField, ValidationError
 from rest_framework.fields import CharField
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
 from apps.users.models import User
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
